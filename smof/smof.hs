5b9409d16918e760396497cd25546cb28834213f
Zebulun Arendsee <arendsee@iastate.edu>
2016-10-04 00:37:07 -0500

    My first Haskell runtime error!

--- smof/smof.hs
+++ smof/smof.hs
@@ -1,11 +1,14 @@
 import System.Environment (getArgs)
+import Data.List
+import Data.List.Split
+
 
 -- Borrowed verbatim from Real World Haskell ------------------------
 interactWith function inputFile outputFile = do
   input <- readFile inputFile
   writeFile outputFile (function input)
 
-main = mainWith reverse'
+main = mainWith $ (show . head' . splitSeqs)
   where mainWith function = do
           args <- getArgs
           case args of
@@ -13,17 +16,43 @@ main = mainWith reverse'
             _ -> putStrLn "error: exactly two arguments needed"
 ---------------------------------------------------------------------
 
-data Header = Seqid Description
+data Header = Header String String deriving Show
 
-data Sequence = Protein | DNA | RNA
+data Sequence = UNK String | AA String | DNA String | RNA String deriving Show
 
-data Entry = Header Sequence
+data Entry = Entry Header Sequence deriving Show
 
--- parallel processes, one entry to one result, 1-n-n-1
--- NOTE: this also allows 1-n-m-1, i.e. lengths of [FastaEntries] needn't equal length of [a]
-smof_p :: String -> (String -> [Entry]) -> ([Entry] -> [a]) -> ([a] -> b) -> b
-smof_p cs d p r = (r . p . d) cs  -- same as dpr, but less polymorphic
+splitRecords :: String -> [String]
+splitRecords [] = []
+splitRecords s = splitOn ">" s
 
--- linear processes, all entries to one result, 1-n-1
-smof_l :: String -> (String -> [Entry]) -> ([Entry] -> a) -> a
-smof_l cs d p = (d . p) cs
+makeHead :: String -> Header
+makeHead s = case (words s) of
+    []     -> Header "" ""
+    (x:xs) -> Header x (unwords xs)
+
+makeBody :: [String] -> Sequence
+makeBody [] = UNK ""
+makeBody s = UNK (unlines s)
+
+extractSeq :: String -> Entry
+extractSeq s = case (lines s) of
+    (x:xs) -> Entry (makeHead x) (makeBody xs)
+
+splitSeqs :: String -> [Entry]
+splitSeqs [] = []
+splitSeqs xs = (map extractSeq . splitRecords) xs
+
+head' :: [Entry] -> Maybe Entry
+head' [] = Nothing
+head' (x:xs) = Just x
+
+
+{- -- parallel processes, one entry to one result, 1-n-n-1                                       -}
+{- -- NOTE: this also allows 1-n-m-1, i.e. lengths of [FastaEntries] needn't equal length of [a] -}
+{- smof_p :: String -> (String -> [Entry]) -> ([Entry] -> [a]) -> ([a] -> b) -> b                -}
+{- smof_p cs d p r = (r . p . d) cs  -- same as dpr, but less polymorphic                        -}
+{-                                                                                               -}
+{- -- linear processes, all entries to one result, 1-n-1                                         -}
+{- smof_l :: String -> (String -> [Entry]) -> ([Entry] -> a) -> a                                -}
+{- smof_l cs d p = (d . p) cs                                                                    -}
