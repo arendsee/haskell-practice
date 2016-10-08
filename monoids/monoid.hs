2e19e821f05afacca0df75c7ec922d9d26b881f6
Zebulun Arendsee <arendsee@iastate.edu>
2016-10-08 03:03:31 -0500

    Add broken monoid

---
+++ monoids/monoid.hs
@@ -0,0 +1,21 @@
+-- A monoid is defined simply as a set with both an identity function and an
+-- associative binary operator.
+
+class Monoid a where
+    -- identity element
+    monoid_id :: a
+    -- binary operator
+    monoid_op :: a -> a -> a
+
+data Clock = Clock Int Int deriving Show
+
+
+instance Monoid (Clock h m) where
+    monoid_id (Clock 24 0)
+    monoid_op (Clock h1 m1) (Clock h2 m2) = Clock (mod ht 24) (mod mt 60) where
+        mt = m1 + m2
+        ht = h1 + h2 + div mt 60
+
+main = do
+    print $ Clock 23 4
+    print $ monoid_op (Clock 23 4) (Clock 1 59)
