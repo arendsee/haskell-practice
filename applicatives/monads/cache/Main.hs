6f190897765afa862f411aeb2a6da95a1630f8bd
Zebulun Arendsee <arendsee@iastate.edu>
2016-10-23 17:10:50 -0500

    Add untested merge and branch functions

--- applicatives/monads/cache/Main.hs
+++ applicatives/monads/cache/Main.hs
@@ -1,17 +1,10 @@
-words' :: String -> [String]
-words' = words
-
-reverse' :: [a] -> [a]
-reverse' = reverse
-
-unwords' :: [String] -> String
-unwords' = unwords
-
-data Cached a b = Empty (a -> b) | Full (a -> b) b
+data Cached a b =
+    Empty (a -> b)   |
+    Full  (a -> b) b
 
 instance Show (Cached a b) where
-    show (Empty _) = "empty"
-    show (Full _ b) = "full"
+    show (Empty _  ) = "empty"
+    show (Full  _ b) = "full"
 
 evaluate' :: a -> Cached a b -> b
 evaluate' a (Empty f  ) = f a
@@ -31,10 +24,35 @@ pipe' (Full f x) (Empty g) = Full (g . f) (g x)
 -- if both are full, then return the cached composition
 pipe' (Full f x) (Full g y) = Full (g . f) y
 -- what to do in this situation is a design decision, there are several
--- reasonable choises, I choose the safe route 
+-- reasonable choices, I choose the safe route 
 pipe' (Empty f) (Full g y) = Empty (g . f)
 -- and infix:
 a |. b = a `pipe'` b
+
+getF :: Cached a b -> a
+getF (Empty  f  ) = f
+getF (Cached f _) = f
+
+--------------------------------------------------------------------------
+compose2 :: (a -> b) -> (b -> c) -> (b -> d) -> (a -> ((a -> c), (a -> d)))
+compose2 f g h = \x -> ((g . f) x, (h . f) x)
+
+branch' :: Cached a b -> Cached b c -> Cached b d -> Cached a (c,d)
+branch' (Empty f  ) c1 c2 = Empty (compose2 f g h) where
+    g = getF c1
+    h = getF c2
+branch' (Full  f x) c1 c2 = Full  (compose2 f g h) (g', h') where
+    g  = getF c1
+    h  = getF c2
+    g' = run' c1 x
+    h' = run' c2 x
+
+--------------------------------------------------------------------------
+merge' :: (b -> d -> e) -> Cached a b -> Cached c d -> Cached (b,d) e
+merge' f (Empty g  ) (Empty h  ) = Empty (\x,y -> f (g x) (h y))
+merge' f (Full  g x) (Empty h  ) = Empty (\y   -> f x     (h y))
+merge' f (Full  g x) (Full  h y) = Empty (f x y)
+
 
 --------------------------------------------------------------------------
 
