73d80f07a32df6760e591e1a6c7705cd1b004781
Zebulun Arendsee <arendsee@iastate.edu>
2017-05-25 13:07:24 -0500

    Add list monad example

--- applicatives/monads/do/do.hs
+++ applicatives/monads/do/do.hs
@@ -1,3 +1,5 @@
+import Control.Monad (guard)
+
 -- A silly add function that fails for values greater than 100
 boundAdd :: Int -> Int -> Maybe Int
 boundAdd x y
@@ -6,6 +8,7 @@ boundAdd x y
 
 main :: IO ()
 main = do
+  -- inside a Maybe monad
   print $ do
     x1 <- 10 `boundAdd` 5
     x2 <- x1 `boundAdd` 10
@@ -14,3 +17,18 @@ main = do
                        -- return of Nothing, even x2 would
                        -- be a `Just 25` otherwise.
     return x2
+
+  -- inside a list monad
+  print $ do
+    x1 <- [1,2,3]
+    x2 <- ["Jes","Jac","Jen"]
+    guard $ x1 /= 1
+    return (x1,x2)
+
+  -- Why does this do this? Basically, the list monad turns this into a nested
+  -- for loop. Since the second value is ignored, this ends up just repeating
+  -- the elements in the first vector.
+  print $ do
+    x1 <- [1,2,3]
+    [222,45]
+    return x1
