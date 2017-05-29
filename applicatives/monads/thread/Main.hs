efcd7d37a5b5714b71c6e9ff592e8bf60d68c2ce
Zebulun Arendsee <arendsee@iastate.edu>
2017-05-29 03:09:21 -0500

    Generalize thread so error can be any monoid

--- applicatives/monads/thread/Main.hs
+++ applicatives/monads/thread/Main.hs
@@ -1,13 +1,14 @@
 import Control.Monad
+import Data.Monoid
 
 -- | Passes along two possible states, for success and failure.  A failure
 -- prevents future processing, propagating the final message.
-data Thread a = Thread (Either String (String, a)) deriving(Show)
-instance Monad Thread where
-  return x = Thread (Right ("", x)) 
+data Thread e o = Thread (Either e (e, o))
+instance Monoid e => Monad (Thread e) where
+  return x = Thread (Right (mempty, x)) 
   -- concatenate logs (stderr) and transform result
   Thread (Right (e1, x)) >>= f = case f x of
-    (Thread (Right (e2, x2))) -> Thread $ Right (e1 ++ e2, x2)
+    (Thread (Right (e2, x2))) -> Thread $ Right (e1 <> e2, x2)
     r2 -> r2
   -- propagate failure
   Thread (Left e1) >>= _ = Thread (Left e1)
@@ -15,18 +16,22 @@ instance Monad Thread where
 -- Due to the Applicative Monad Proposal, which requires all Monads be explicit
 -- instances of Applicative andFunctor, just defining the monad 
 
-instance Functor Thread where
+instance Monoid e => Functor (Thread e) where
   fmap = liftM
 
-instance Applicative Thread where
+instance Monoid e => Applicative (Thread e) where
   pure = return
   (<*>) = ap
 
-trydivide :: Double -> Double -> Thread Double
+instance (Show e, Show o) => Show (Thread e o) where
+  show (Thread (Left err)) = show err ++ "\n *** FAILURE ***"
+  show (Thread (Right (_, out))) = show out
+
+trydivide :: Double -> Double -> Thread String Double
 trydivide y x
   | y == 0 = Thread (Left errmsg)
   | otherwise = Thread (Right (sucmsg, x / y)) where
-  errmsg = "FAILURE: division by 0\n"
+  errmsg = "ERROR: division by 0\n"
   sucmsg = unwords ["SUCCESS: divided ", show x, " by ", show y, "\n"]
 
 main :: IO ()
