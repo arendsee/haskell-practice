ef22dc317a48c5309e53d21875ca42a242e0a399
Zebulun Arendsee <arendsee@iastate.edu>
2017-06-07 17:09:47 -0500

    Add serialization to/from files
     #serialization #unsafe #encodeFile #decodeFile

--- serialization/Main.hs
+++ serialization/Main.hs
@@ -16,3 +16,21 @@ main = do
   putStrLn $ show f
   putStrLn $ show b
   putStrLn $ show d
+
+  -- equivalent to `ByteString.writeFile "zzz.dat" . encode $ f`
+  DB.encodeFile "zzz.dat" f
+
+  -- NOTE: we have to provide a type signature when reading binary
+  f' <- DB.decodeFile "zzz.dat" :: IO FooBar
+
+  putStrLn $ show f'
+
+  -- We could provide the wrong data type
+  f'' <- DB.decodeFile "zzz.dat" :: IO Int
+
+  -- So we can happily cast the data to anything, which in this case is a valid
+  -- integer.
+  putStrLn $ show f''
+
+  -- In other cases it might lead to a runtime error. In other words, this is a
+  -- kind of scary thing to do and should be used with care.
