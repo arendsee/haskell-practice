e7cdaddfac9f202761b8496e29ce42c63671157f
Zebulun Arendsee <arendsee@iastate.edu>
2018-09-27 18:10:52 -0500

    Notes on Arrow and function instances

--- arrows/README.md
+++ arrows/README.md
@@ -2,6 +2,10 @@
 
 Arrows are a generalization of monads.
 
+Arrows generalize functions. The Arrow instance of `->` is just function
+application. More generally, though, an Arrow instance can be application with
+some effect. An Arrow is a relation between an input and output.
+
 # Material
 
  1. `https://en.wikibooks.org/wiki/Haskell/Arrow_tutorial` - Practical
@@ -9,3 +13,5 @@ Arrows are a generalization of monads.
     this once I have a better theoretical idea of what is going on.
 
  2. `https://www.haskell.org/arrows/` -  a brief but good introduction and has references.
+
+ 3. `https://wiki.haskell.org/Typeclassopedia#Arrow` - of course typeclassopedia has a good discussion of arrows.
--- arrows/arrows.hs
+++ arrows/arrows.hs
@@ -1,11 +1,39 @@
--- from: https://www.haskell.org/arrows/
-
 class Arrow a where
-  -- Each function may be treated as a computation.
+  -- Raise a function into a Arrow context
   arr :: (b -> c) -> a b c
 
-  -- Computations may be composed, by connecting the output of the first to the input of the second.
+  -- Apply a computation to the first part of a value, passing the rest through unchanged
+  first :: a b c -> a (b,d) (c,d)
+
+  -- Generalized composition operator
   (>>>) :: a b c -> a c d -> a b d
 
-  -- A computation may be applied to part of the input, with the rest copied through to the output.
-  first :: a b c -> a (b,d) (c,d)
+  -- Parallel computation operator
+  (***) :: a b c -> a b' c' -> a (b, b') (c, c')
+
+  -- Fanout operator
+  (&&&) :: a b c -> a b c' -> a b (c, c')
+
+
+instance Arrow (->) where
+  -- arr :: (b -> c) -> a b c
+  -- arr :: (b -> c) -> (->) b c
+  -- arr :: (b -> c) -> (b -> c)
+  arr = id
+
+  -- first :: a b c -> a (b,d) (c,d)
+  -- first :: (b -> c) -> ((b,d) -> (c,d))
+  -- first :: (b -> c) -> (b,d) -> (c,d)
+  first f (x,y) = (f x, y) 
+
+  -- (>>>) :: a b c -> a c d -> a b d
+  -- (>>>) :: (b -> c) -> (c -> d) -> (b -> d)
+  (>>>) :: flip (.)
+
+  -- (***) :: a b c -> a b' c' -> a (b b') (c c')
+  -- (***) :: (b -> c) -> (b' -> c') -> (b, b') -> (c, c')
+  (***) f g (x, y) = (f x, g y)
+
+  -- (&&&) :: a b c -> a b c' -> a b (c, c')
+  -- (&&&) :: (b -> c) -> (b -> c') -> b -> (c, c')
+  (&&&) f g x = (f x, g x)
